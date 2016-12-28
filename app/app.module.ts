b31c1662547e6ab00b0e6fb58c64eb8f4138293a
Danilo Del Fio <ddelfio@gmail.com>
2016-12-28 05:27:51 +0100

    First commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ 
+    BrowserModule,
+    HttpModule,
+    FormsModule
+    ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
